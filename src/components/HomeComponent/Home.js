4d0577a8f922e5f51f494ffb35f5a1e2cd1efc7b
DannyZucho <danny.nico.dev@outlook.com>
2021-06-10 17:13:09 -0700

    Practice - start hooks and props

---
+++ src/components/HomeComponent/Home.js
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import BlogList from '../blogListComponent/BlogList'
+import './home.style.css'
+
+const Home = () => {
+  // hook -- useState hook
+  // >> gives us a way to make a reactive value and provides us a way to change that value whenever we want
+  /**
+  const [name, setName] = useState('mario')
+  const [age, setAge] = useState(25)
+  */
+  // const handleClick = () => {
+  //   if (name === 'mario') {
+  //     setName('luigi')
+  //     setAge(30)
+  //   } else if (name === 'luigi') {
+  //     setName('mario')
+  //     setAge(25)
+  //   }
+  // }
+
+  const [blogs, setBlogs] = useState([
+    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+    {
+      title: 'Web dev top tips',
+      body: 'lorem ipsum...',
+      author: 'mario',
+      id: 3,
+    },
+  ])
+
+  return (
+    <div className="home">
+      <div>
+        <BlogList blogs={blogs} title="All Blogs" />
+      </div>
+    </div>
+  )
+}
+
+export default Home
