01b6b2c119dd9e0152bb9fc4771854c6baf0963f
DannyZucho <danny.nico.dev@outlook.com>
2021-06-25 03:17:27 -0700

    Create inputs for create post page

--- src/components/createComponent/Create.js
+++ src/components/createComponent/Create.js
@@ -1,7 +1,30 @@
+import { useState } from 'react'
+
 const Create = () => {
+  const [title, setTitle] = useState('')
+
   return (
     <div className="create">
       <h2>Create a new post!</h2>
+      <form>
+        <label>Blog title: </label>
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+
+        <label>Blog body: </label>
+        <textarea required></textarea>
+
+        <label>Blog author: </label>
+        <select>
+          <option value="mario">mario</option>
+          <option value="yoshi">yoshi</option>
+        </select>
+        <button>Add Blog</button>
+      </form>
     </div>
   )
 }
